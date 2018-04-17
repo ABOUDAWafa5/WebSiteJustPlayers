96d1d2229e3f39bf92e5a5205db8ec264035ba80
ENDAVA\vgerasimov <vasil.gerasimov@endava.com>
2018-04-17 18:04:05 +0300

    add tournament search

---
+++ ClanWebSite/ClanWebSite/Common/InMemoryCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+using System.Web.Caching;
+
+namespace ClanWebSite.Common
+{
+
+    public class InMemoryCache : ICacheService
+    {
+        public T GetOrSet<T>(string cacheKey, Func<T> getItemCallback) where T : class
+        {
+            if (!(MemoryCache.Default.Get(cacheKey) is T item))
+            {
+                item = getItemCallback();
+                MemoryCache.Default.Add(cacheKey, item, DateTime.Now.AddMinutes(2));
+            }
+            return item;
+        }
+    }
+
+    interface ICacheService
+    {
+        T GetOrSet<T>(string cacheKey, Func<T> getItemCallback) where T : class;
+    }
+}
